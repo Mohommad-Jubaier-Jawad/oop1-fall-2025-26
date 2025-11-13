73c4d07a2cc542444f81a934f05da21e2c409bd7
Mohommad-Jubaier-Jawad <jjmahin12@gmail.com>
2025-11-13 22:36:08 +0600

    25-60638-1

---
+++ Task2.java
@@ -0,0 +1,18 @@
+package Lab;
+
+
+public class Task2 {
+    public static void main (String[] args ){
+
+    
+
+    double price = 250 ;
+    double discount = 0.15;
+    double orignal = price * discount;
+    double  showprice = price - orignal;
+    System.out.print("Price after discount="+showprice); 
+
+    
+}
+  
+}
